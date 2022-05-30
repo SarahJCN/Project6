e08cf66f76bfa1d56fac45554d11382a9ef1e36e
jellocan <christopherle.guelph@gmail.com>
2022-05-30 10:26:16 -0400

    AYYYOOO

--- webpage/index.php
+++ webpage/index.php
@@ -35,25 +35,25 @@
 
 <html>
 	<div class="MainC">
-
-	</div>
 	<h1>Elevator Control</h1> 
 	
-		<?php 
-			if(isset($_POST['newfloor'])) {
-				$curFlr = update_elevatorNetwork(1, $_POST['newfloor']); 
-				header('Refresh:0; url=index.php');	
-			} 
-			$curFlr = get_currentFloor();
-			echo "<h2>Current floor # $curFlr </h2>";			
-		?>		
-		
-		<h2> 	
-			<form action="index.php" method="POST">
-				Request floor # <input type="number" style="width:50px; height:40px" name="newfloor" max=3 min=1 required />
-				<input type="submit" value="Go"/>
-			</form>
-		</h2>
+	<?php 
+		if(isset($_POST['newfloor'])) {
+			$curFlr = update_elevatorNetwork(1, $_POST['newfloor']); 
+			header('Refresh:0; url=index.php');	
+		} 
+		$curFlr = get_currentFloor();
+		echo "<h2>Current floor # $curFlr </h2>";			
+	?>		
+	
+	<h2> 	
+		<form action="index.php" method="POST">
+			Request floor # <input type="number" style="width:50px; height:40px" name="newfloor" max=3 min=1 required />
+			<input type="submit" value="Go"/>
+		</form>
+	</h2>
+	</div>
+	
 		  
 		
 </html>
