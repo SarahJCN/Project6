5b878a61257373696d10c09fac7da0d65084ffbe
jellocan <christopherle.guelph@gmail.com>
2022-05-30 10:26:54 -0400

    AYYYOOO

--- webpage/index.php
+++ webpage/index.php
@@ -30,7 +30,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project 6 landing page</title>
-    <link href="./CSS/style0.css" type="text/css" rel="stylesheet">
+    <link href="./CSS/style2.css" type="text/css" rel="stylesheet">
 </head>
 
 <html>
