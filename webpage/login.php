d9abc62f0b18c6b1d367caaa055d463f8c10a87c
Tori Le <tle5840@conestogac.on.ca>
2022-06-13 08:13:39 -0400

    Fixed login.php.

--- webpage/login.php
+++ webpage/login.php
@@ -6,7 +6,7 @@ $submitted = !empty($_POST);
 	<head><title>Form Handler Page</title></head>
 	<body>
 		<p>Form submitted? <?php echo (int) $submitted; ?> </p>
-		<p>Your login info is</p>
+		<p>Your login info submitted is</p>
 		<ul>
 			<li><b>username</b>: <?php echo $_POST['username']; ?></li>
 			<li><b>password</b>: <?php echo $_POST['password']; ?></li>
