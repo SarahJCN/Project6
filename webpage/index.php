d6a16cab4b0df91dec51f16524dfd94f8f6bbfed
Tori Le <tle5840@conestogac.on.ca>
2022-06-18 22:58:26 -0400

    Edited index.php

--- webpage/index.php
+++ webpage/index.php
@@ -1,11 +1,21 @@
 <?php
 	function update_elevatorNetwork(int $node_ID, int $new_floor =1): int {
+		$dir = "";
+		$sig = "";
+		if($new_floor > 1 && $new_floor < 3)
+			$dir = "Up";
+		else if($new_floor == 3)
+			$dir = "Up";
+		else
+			$dir = "Down";
 		$db1 = new PDO('mysql:host=127.0.0.1;dbname=elevator','ese','ese');
 		$query = 'UPDATE elevatorNetwork 
 				SET currentFloor = :floor
+				direction = :dir
 				WHERE nodeID = :id';
 		$statement = $db1->prepare($query);
 		$statement->bindvalue('floor', $new_floor);
+		$statement->bindvalue('dir', $dir);
 		$statement->bindvalue('id', $node_ID);
 		$statement->execute();	
 		
@@ -24,6 +34,18 @@
 			}
 			return $current_floor;
 	}
+
+	function get_direction() {
+		try { $db = new PDO('mysql:host=127.0.0.1;dbname=elevator','ese','ese');}
+		catch (PDOException $e){echo $e->getMessage();}
+
+			// Query the database to display current direction
+			$rows = $db->query('SELECT direction FROM elevatorNetwork');
+			foreach ($rows as $row) {
+				$direction = $row[0];
+			}
+			return $direction
+	}
 ?>
 <head>
     <meta charset="UTF-8">
@@ -31,6 +53,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project 6 landing page</title>
     <link href="./CSS/style2.css" type="text/css" rel="stylesheet">
+	<script type="text/javascript" src="js/jquery.js"></script>
 </head>
 
 <html>
