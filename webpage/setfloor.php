8648f9723ca92c21e8687ebf924a55f69d464324
Tori Le <tle5840@conestogac.on.ca>
2022-07-04 07:45:26 -0400

    Update

--- webpage/setfloor.php
+++ webpage/setfloor.php
@@ -9,10 +9,10 @@
 	}
 	
 	$db1 = mysqli_connect("localhost","ese","ese","elevator");
-	$query = "UPDATE elevatornetwork SET currentFloor = '$fno' WHERE nodeID = '1'";
+	$query = "UPDATE elevatorNetwork SET currentFloor = '$fno' WHERE nodeID = '1'";
 	mysqli_query($db1, $query) or die(mysqli_error($db1));
 
-		$qry = "SELECT currentFloor FROM elevatornetwork where nodeID = 1";
+		$qry = "SELECT currentFloor FROM elevatorNetwork where nodeID = 1";
 		$data = mysqli_query($db1, $qry) or die(mysqli_error($db1));
 		$row = mysqli_fetch_array($data);
 		echo $row["currentFloor"];
