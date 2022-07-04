b2b3169f5668824b6c55069d9908d75037919497
Tori Le <tle5840@conestogac.on.ca>
2022-07-04 06:52:55 -0400

    Added Setfloor.php

---
+++ webpage/setfloor.php
@@ -0,0 +1,19 @@
+<?php 
+	$fno = $_POST["fno"];
+	$flag = $_POST['flag'];
+	//echo "(".$fno." ".$flag.")";
+	if($flag == "up" && $fno < 3)
+		$fno++;
+	if($flag == "down" && $fno > 1){
+		$fno--;
+	}
+	
+	$db1 = mysqli_connect("localhost","root","","elevator");
+	$query = "UPDATE elevatornetwork SET currentFloor = '$fno' WHERE nodeID = '1'";
+	mysqli_query($db1, $query) or die(mysqli_error($db1));
+
+		$qry = "SELECT currentFloor FROM elevatornetwork where nodeID = 1";
+		$data = mysqli_query($db1, $qry) or die(mysqli_error($db1));
+		$row = mysqli_fetch_array($data);
+		echo $row["currentFloor"];
+ ?>
