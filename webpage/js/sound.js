5b2fbb50718c9837c1637cb6607fdb5eda3121df
Tori Le <tle5840@conestogac.on.ca>
2022-07-18 07:51:54 -0400

    Fixed sound

--- webpage/js/sound.js
+++ webpage/js/sound.js
@@ -2,19 +2,19 @@
 // 			myAudio.play();
 
 function sound1(){
-	var myAudio = new Audio('js/test.mp3'); 
+	var myAudio = new Audio('js/floor1.mp3'); 
 			myAudio.play();
-	$(".myBell").css({"animation":"ring 2s"});
+			$(".myBell").css({"animation":"ring 2s"});
 }
 
 function sound2(){
-	var myAudio = new Audio('js/test.mp3'); 
+	var myAudio = new Audio('js/floor2.mp3'); 
 			myAudio.play();
 			$(".myBell").css({"animation":"ring 2s"});
 }
 
 function sound3(){
-	var myAudio = new Audio('js/test.mp3'); 
+	var myAudio = new Audio('js/floor3.mp3'); 
 			myAudio.play();
 			$(".myBell").css({"animation":"ring 2s"});
 }
